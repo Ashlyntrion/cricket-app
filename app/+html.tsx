26ec8b1d041e7946e800cd91e2e057d3e37b8b17
ashlyn <ashlynramesh@gmail.com>
2026-06-10 00:27:13 +0530

    PWA: manifest + iOS meta tags for home screen install

---
+++ app/+html.tsx
@@ -0,0 +1,32 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        {/* PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#22c55e" />
+
+        {/* iOS "Add to Home Screen" support */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Cricket Coach" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+
+        {/* Prevent text size adjustment on orientation change */}
+        <style>{`
+          html { -webkit-text-size-adjust: 100%; }
+          body { margin: 0; padding: 0; }
+        `}</style>
+
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
